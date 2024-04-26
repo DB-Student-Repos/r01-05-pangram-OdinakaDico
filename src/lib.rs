6f54a5997f1df926df83b2d229795109badda686
OdinakaDico <149073751+OdinakaDico@users.noreply.github.com>
2024-04-26 19:17:50 +0200

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
 /// Determine whether a sentence is a pangram.
 pub fn is_pangram(sentence: &str) -> bool {
-    unimplemented!("Is {sentence} a pangram?");
+    let mut alphabet = [false; 26]; 
+    for c in sentence.chars().filter(|c| c.is_ascii_alphabetic()) {
+        let index = c.to_ascii_lowercase() as usize - 'a' as usize;
+        alphabet[index] = true;
+    }
+    alphabet.iter().all(|&present| present)
 }
